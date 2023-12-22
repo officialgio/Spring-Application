ed0b24a4f1a344913d3970db336f7a60471efa7e
officialgio121 <officialgio121@gmail.com>
2023-12-22 00:35:50 -0600

    Added Dependency Injection

--- demo/src/main/java/com/example/demo/customer/CustomerController.java
+++ demo/src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,10 +1,18 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     public Customer getCustomer() {
         return new Customer(1L, "Giovanny Hernandez");
--- demo/src/main/java/com/example/demo/customer/CustomerService.java
+++ demo/src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,4 +1,8 @@
 package com.example.demo.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component // Creates a bean (single instance)
 public class CustomerService {
+
 }
